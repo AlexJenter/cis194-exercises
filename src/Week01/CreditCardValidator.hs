422d9bfbef8971a33dc56f8f7eeaabfb8d523fee
AlexJenter <jenteralex@gmail.com>
2020-03-24 09:40:28 +0100

    Works but could be way more stupid

--- src/Week01/CreditCardValidator.hs
+++ src/Week01/CreditCardValidator.hs
@@ -16,13 +16,15 @@ toDigitsRev x
   where (initialDigits, lastDigit) = divMod x 10
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther []  = []
-doubleEveryOther [x] = [x]
+doubleEveryOther []       = []
+doubleEveryOther [x]      = [x]
 doubleEveryOther (x:y:ys) = x * fx : y * fy : doubleEveryOther ys
   where (fx, fy) = if (even $ length ys) then (2,1) else (1,2)
 
 sumDigits :: [Integer] -> Integer
-sumDigits = error "Week01.CreditCardValidator#sumDigits not implemented"
+sumDigits []     = 0
+sumDigits [x]    = x
+sumDigits (x:xs) = (sumDigits $ toDigits x) + sumDigits xs
 
 validate :: Integer -> Bool
-validate = error "Week01.CreditCardValidator#validate not implemented"
+validate n = (sumDigits $ doubleEveryOther $ toDigits n) `mod` 10 == 0
