b607cb2641a87502c530a8c17a7f09ce18d4d590
Alex Jenter <jenteralex@gmail.com>
2020-04-02 01:44:50 +0200

    best way to ruin a good walk

--- src/Week03/Golf.hs
+++ src/Week03/Golf.hs
@@ -5,11 +5,32 @@ module Week03.Golf
   )
 where
 
+import           Data.List
+
 skips :: [a] -> [[a]]
-skips = error "Week03.Golf#skips not implemented"
+skips xs = [ [ xs !! (x - 1) | x <- range, mod x y == 0 ] | y <- range ]
+  where range = [1 .. length xs]
+
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = error "Week03.Golf#localMaxima not implemented"
+localMaxima xs = map second $ filter isPeak (zip3 xs xs' xs'')
+ where
+  xs'  = tail xs
+  xs'' = tail xs'
+  isPeak (x, y, z) = x < y && y > z
+  second (_, x, _) = x
+
 
 histogram :: [Integer] -> String
-histogram = error "Week03.Golf#histogram not implemented"
+histogram xs = unlines (reverse . transpose $ map f [0 .. 9]) ++ chrome
+ where
+  bins   = group $ sort xs
+  lens   = map length bins
+  reco   = maximum lens
+  chrome = "==========\n0123456789\n"
+
+  f :: Integer -> String
+  f i = case i `elemIndex` map head bins of
+    Just n -> replicate len '*' ++ replicate (reco - len) ' '
+      where len = lens !! n
+    Nothing -> replicate reco ' '
