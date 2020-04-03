d5f0cd5c0599c46410fa272a67324cb7cd8e49eb
Alex Jenter <jenteralex@gmail.com>
2020-04-03 16:19:47 +0200

    refactor skips to be more readable

--- src/Week03/Golf.hs
+++ src/Week03/Golf.hs
@@ -8,9 +8,15 @@ where
 import           Data.List
 
 skips :: [a] -> [[a]]
-skips xs = [ [ xs !! (x - 1) | x <- range, mod x y == 0 ] | y <- range ]
-  where range = [1 .. length xs]
+skips xs = [ everyNth n xs | n <- [1 .. length xs] ]
+ where
+  everyNth :: Int -> [a] -> [a]
+  everyNth = everyNth' 1
 
+  everyNth' :: Int -> Int -> [a] -> [a]
+  everyNth' _ _ [] = []
+  everyNth' i n' (z : zs) | i == n'   = z : everyNth' 1 n' zs
+                          | otherwise = everyNth' (i + 1) n' zs
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima xs = map second $ filter isPeak (zip3 xs xs' xs'')
@@ -22,15 +28,20 @@ localMaxima xs = map second $ filter isPeak (zip3 xs xs' xs'')
 
 
 histogram :: [Integer] -> String
-histogram xs = unlines (reverse . transpose $ map f [0 .. 9]) ++ chrome
+histogram xs =
+  unlines (reverse . transpose $ map fn [0 .. 9]) ++ "==========\n0123456789\n"
  where
-  bins   = group $ sort xs
-  lens   = map length bins
-  reco   = maximum lens
-  chrome = "==========\n0123456789\n"
+  bins    = group $ sort xs
+  lengths = map length bins
+  record  = maximum lengths
 
-  f :: Integer -> String
-  f i = case i `elemIndex` map head bins of
-    Just n -> replicate len '*' ++ replicate (reco - len) ' '
-      where len = lens !! n
-    Nothing -> replicate reco ' '
+  fn :: Integer -> String
+  fn i = case i `elemIndex` map head bins of
+    Nothing -> replicate record ' '
+    Just n -> replicate lenStar '*' ++ replicate lenSpace ' '
+      where
+        lenStar = lengths !! n
+        lenSpace = record - lenStar
+
+
+
